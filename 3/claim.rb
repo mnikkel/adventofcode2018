565f95bc53a20fefb4ceb228cd0c3adbabef5127
Mark Nikkel <mnikkel@gmail.com>
2018-12-04 17:49:17 +0000

    Day 3 part 2

--- 3/claim.rb
+++ 3/claim.rb
@@ -19,3 +19,15 @@ claims.each do |claim|
 end
 
 puts claim_hash.values.select { |v| v > 1 }.length
+
+claims.each do |claim|
+  x_off, y_off = claim[0].split(",").map(&:to_i)
+  x, y = claim[1].split("x").map(&:to_i)
+  claim_count = []
+  (1..y).each do |i|
+    (1..x).each do |j|
+      claim_count << claim_hash[[x_off+j, y_off+i]]
+    end
+  end
+  p [x_off, y_off, x, y] if claim_count.all? { |el| el == 1 }
+end
